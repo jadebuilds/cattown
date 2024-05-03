21ca3577398eddf7145ec09b39180589b2c9f690
Chris Sarantos <csarantos@gmail.com>
2024-05-02 19:01:42 -0700

    reopen live stream if it closes unexpectedly, other tweaks

--- pi_controller/cv/test_rpi_algo.py
+++ pi_controller/cv/test_rpi_algo.py
@@ -3,16 +3,17 @@
 import numpy as np
 import cv2 as cv
 import os
+import time
+
 # import matplotlib.pyplot as plt
-# %matplotlib widget
 
 # Stored video file location
 fdir = '/home/chris/cattown/'
 fname = '2024-03-22 first trial.mov'
 
-# TODO: Make these input arguments
+# TODO: Make these input arguments?
 WRITE_OUTPUT = False
-READ_FILE = False
+READ_FILE = False # setting this to false means use live video from attached camera (USB or otherwise)
 read_livecam_id = 0
 SHOW_ORIENTATION = True
 SHOW_CONTOUR = False
@@ -57,16 +58,22 @@ def show_frame(frame):
 	with open('/dev/fb0', 'rb+') as buf:
 		buf.write(frame_c)
 
+def open_live_capture(id):
+	return cv.VideoCapture(id)
 
-# Magic numbers:
+
+
+# =================== Magic numbers ===================
 # minimum and maximum object contour area to be tracked:
 # TODO: Calculate these after Aruco transform to put in real units (cm^2)
+# These will need updating regardless, just guesses based on original test camera and video
 min_contour_area = 2000
-max_contour_area = 10000
+max_contour_area = 20000
 
 
 backsub_thresh = 180 # Threshold for movement in background subtracted image
 
+# Leftover from reading test video file:
 # Process only subset of video:
 frame_start = 300
 frames_to_proc = 450
@@ -75,7 +82,7 @@ frames_to_proc = 450
 # maxvar_scaling = 50 # old method
 maxvar_scaling = 0.02 # Since we don't divide x,y by their stdev, things get large
 
-# Attempt manual mtion tracking method - background subtraction and contours:
+# Attempt manual motion tracking method - background subtraction and contours:
 back_sub = cv.createBackgroundSubtractorMOG2()
 
 frame_end = frame_start + frames_to_proc
@@ -86,8 +93,7 @@ if READ_FILE:
 	# Advance the capture to skip the first frames we don't want:
 	cap.set(cv.CAP_PROP_POS_FRAMES, frame_start)
 else:
-	frames_to_proc = np.inf
-	cap = cv.VideoCapture(read_livecam_id)
+	cap = open_live_capture(read_livecam_id)
 
 # Configure output stream:
 frame_width = int(cap.get(3))
@@ -102,17 +108,22 @@ if WRITE_OUTPUT:
 	if not out.isOpened():
 		raise ValueError('VideoWriter failed to initialize')
 
-while cap.isOpened():
+while True:
+	if not cap.isOpened():
+		print('Live camera capture failed to open, trying again...')
+		cap = open_live_capture(read_livecam_id) 
+
 	frame_num += 1
 	
-	if frame_num > frame_end:
+	if READ_FILE and frame_num > frame_end:
 		break
 	
 	# Capture frame-by-frame
 	ret, frame = cap.read()
 	if not ret:
-		print("Can't receive frame (stream end?). Exiting ...")
-		break
+		print("Can't receive frame (stream end?).  Trying again")
+		# break # let the loop proceed for now, fix crashing issue?
+		continue
 	
 	# Apply background subtraction
 	fg_mask = back_sub.apply(frame)
