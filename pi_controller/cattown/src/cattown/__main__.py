da77f8e92f3e32d728b3613227c6c490be7fc86b
Jade <jade@rootsystems.net>
2024-05-09 02:28:09 -0700

    Sketch basic framework for repo
    Here I propose a basic class structure and interfaces for each module.
    
    This is obviously (as everything I write I think???) super up for discussion --
    just trying to frame things out.
    
    I'm keeping it super simple right now, not doing any swapping, just the
    absolute dumbest most basic sketch of what our different systems might look like.

--- pi_controller/cattown/src/cattown/__main__.py
+++ pi_controller/cattown/src/cattown/__main__.py
@@ -1,11 +1,33 @@
 """Command-line interface."""
 import click
 
+from .platform_mgr import PlatformManager
+from .cv import ComputerVisionSystem
+from .personalities.canned_pattern import DoesCannedPattern
+
+PERSONALITIES = [
+    # todo how does gcode work??? add some real gcode moves.
+    # (I see a text2gcodes() but not gcodes2text() in pygcode... does it only parse and not generate??????)
+    DoesCannedPattern([])
+]
 
 @click.command()
 @click.version_option()
 def main() -> None:
-    """Cat Town."""
+    """Does Cat Town!!!!"""
+
+    platform_manager = PlatformManager()
+    platform_manager.initialize_hardware()
+
+    cv = ComputerVisionSystem()
+    cv.initialize_hardware()
+    
+    platform_manager.deploy_mouse(0)
+    active_personality = PERSONALITIES[0]
+
+    while True:
+        active_personality.update_cat_state(cv.get_cat_state())
+        platform_manager.execute_trajectory(active_personality.get_trajectory())
 
 
 if __name__ == "__main__":
