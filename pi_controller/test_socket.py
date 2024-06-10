9d4d953aca800afc780a38f2783ef30fff1a46e0
Jade <jade@rootsystems.net>
2024-06-09 17:17:34 -0700

    Test out mouse commands
    Update test_socket.py to manually trigger the commands to pick up /
    drop off
    
    Note: I expect to amend this commit a bunch. It should maybe get
    dropped from the history at the end? Lol not likely~

--- pi_controller/test_socket.py
+++ pi_controller/test_socket.py
@@ -1,22 +1,28 @@
 from pi_controller.printer import KlipperSocket
 from pi_controller.motion import DirectMove, RelativeMove, Point, SimpleSquigglePath, MapConfig
+from pi_controller.mice import MouseHouse, PickUpMouse, DropOffMouse
 import time
 
 k = KlipperSocket('/home/fes/printer_data/comms/klippy.sock')
 
-squiggle = SimpleSquigglePath(
-    map_path=[
-        (0, 0), (0, 1), (0, 2), (0, 3),
-        (0, 4), (1, 4), (2, 4), (3, 4),
-        (4, 4), (4, 3), (4, 2), (4, 1),
-        (4, 0), (3, 0), (2, 0), (1, 0),
-        (1, 1), (1, 2), (2, 2), (3, 2), 
-        (3, 3), (3, 4), (4, 4),
-    ],
-    map_config=MapConfig(50., 62., 47.),
-    speed_mm_s=500
-)
+# squiggle = SimpleSquigglePath(
+#     map_path=[
+#         (0, 0), (0, 1), (0, 2), (0, 3),
+#         (0, 4), (1, 4), (2, 4), (3, 4),
+#         (4, 4), (4, 3), (4, 2), (4, 1),
+#         (4, 0), (3, 0), (2, 0), (1, 0),
+#         (1, 1), (1, 2), (2, 2), (3, 2), 
+#         (3, 3), (3, 4), (4, 4),
+#     ],
+#     map_config=MapConfig(50., 62., 47.),
+#     speed_mm_s=500
+# )
 
-k.enqueue_motion(squiggle)
+
+panel_2_house = MouseHouse((19, 5), occupied=False)
+
+k.enqueue_motion(DropOffMouse(panel_2_house))
+
+
 time.sleep(10)
 k.stop()
