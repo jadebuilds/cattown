ddefbf7b7f13311155a5d5d8178c62ca82a6039e
Jade <jade@rootsystems.net>
2024-06-09 22:17:02 -0700

    Add Point-to-Node conversion
    I'm going to stream live location from Moonraker in the hope of
    getting the same update rate that Mainsail features, which is pretty
    good. I think that just converting from the current reported location
    to the nearest tile and saying "I'm here now" is a good way of figuring
    out how far in the gcode queue we've gotten.
    
    Toward that end, add a to_node() function to custommap which converts
    back into map tile location (note that I'm starting to prefer the term
    "tile" since it implies area! ie it's more intuitive to say "where am
    I on the tile?" than it is to say "where am I in the node?").
    
    Include the map size in MapConfig so that I can error-check in the
    conversion function (just in case... I dunno. Just in case)

--- pi_controller/pi_controller/constants.py
+++ pi_controller/pi_controller/constants.py
@@ -4,11 +4,19 @@ from typing import Tuple, List
 @dataclass
 class MapConfig:
     map_grid_spacing_mm: float  # For conversion between map locations (Nodes, int) and physical locations (Points, float)
-    map_x_offset: float  # I think we'll probably need this to calibrate against the physical frame
-    map_y_offset: float  # ""
+    map_x_offset: float  # For calibration against the physical frame -- because 0, 0 for the drive system
+    map_y_offset: float  #     is inset from the left bottom of the board by the frame and mounting brackets.
+    grid_size_x: int  # Size of the map. Assumed to fit within the board -- note that we don't currently
+    grid_size_y: int  # do any size checking! Any tile is assumed to be all valid Points on the board
 
-
-OPEN_SAUCE_MAP_CONFIG = MapConfig(50.0, -137.0, -53.0)  # using a 50 mm spacing
+# Open Sauce map is here: https://docs.google.com/spreadsheets/d/1h3TzB2h-GPvKF6A5ktIeTxZr08u3RLS3S5u_wHjHqy4/edit#gid=1701643471
+OPEN_SAUCE_MAP_CONFIG = MapConfig(
+    map_grid_spacing_mm=50.0, # 100 mm slot grid, divided by two ("on the slot" vs. "between slots")
+    map_x_offset=-137.0,  # Drive system "home" point is 137mm right and 53 mm up
+    map_y_offset= -53.0,  # from the lower left corner of the board
+    grid_size_x=47,  # 6 slots * 4 panels, time two minus one (note that when zero-indexed, as in the Google Sheet, it's 0:46)
+    grid_size_y=21,  # 11 slots vertically per panel, times two minus one
+)
 
 
 # Map values (not using an Enum for the moment because they're so clunky in Python)
