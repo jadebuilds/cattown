df7075c33066d9b325747c3052a8a63bd2df4ddc
Jade <jade@rootsystems.net>
2024-10-19 14:24:36 -0700

    Auto-format infinite_loop.py
    Bring in ruff as a dependency. Auto-format infinte_loop.py
    
    Also bring in nicegui -- that's for later -- sorry bout it

--- pi_controller/pi_controller/infinite_loop.py
+++ pi_controller/pi_controller/infinite_loop.py
@@ -1,15 +1,18 @@
 # manual_control.py
-# 
+#
 # "Fork" of map_display.py which uses the cursor to drive the mouse.
-# 
+#
 # Author: Jade
 
 from typing import Optional, Tuple
 import logging
-logging.getLogger('matplotlib').setLevel(logging.INFO)  # https://stackoverflow.com/questions/35325042/python-logging-disable-logging-from-imported-modules
+
+logging.getLogger("matplotlib").setLevel(
+    logging.INFO
+)  # https://stackoverflow.com/questions/35325042/python-logging-disable-logging-from-imported-modules
 import colorlog
 import matplotlib.pyplot as plt
-from matplotlib.patches import Rectangle  
+from matplotlib.patches import Rectangle
 import matplotlib.animation as animation
 import numpy as np
 import threading
@@ -29,52 +32,59 @@ from .constants import OPEN_SAUCE_MAP_CONFIG
 
 logger = logging.getLogger(__name__)
 
+
 class Game:
     def __init__(self):
         self.lock = threading.Lock()
-        self.path_finder = PathFinder('pi_controller/map.csv')
+        self.path_finder = PathFinder("pi_controller/map.csv")
         self.toolhead = Toolhead(
             # MockMotionDriver(),
-            MoonrakerSocket('cattown001.local'),
+            MoonrakerSocket("cattown001.local"),
             SimpleStraightLines(speed_mm_s=350.0, map_config=OPEN_SAUCE_MAP_CONFIG),
-            #ArcSquiggles(speed_mm_s=500, map_config=OPEN_SAUCE_MAP_CONFIG),
-            OPEN_SAUCE_MAP_CONFIG
+            # ArcSquiggles(speed_mm_s=500, map_config=OPEN_SAUCE_MAP_CONFIG),
+            OPEN_SAUCE_MAP_CONFIG,
         )
         self.toolhead_path = ToolheadTrajectory()
 
         # Initialize the plot
-        self.map = load_from_file('pi_controller/map.csv')
+        self.map = load_from_file("pi_controller/map.csv")
         self.fig, self.ax = plt.subplots()
-        self.cax = self.ax.matshow(self.map, cmap='Grays', origin='lower')
-        self.mouse, = self.ax.plot([], [], 'bo')
-        self.cursor, = self.ax.plot([], [], 'ro')
-        self.path_line, = self.ax.plot([], [], 'b--')
+        self.cax = self.ax.matshow(self.map, cmap="Grays", origin="lower")
+        (self.mouse,) = self.ax.plot([], [], "bo")
+        (self.cursor,) = self.ax.plot([], [], "ro")
+        (self.path_line,) = self.ax.plot([], [], "b--")
 
         self.last_cursor_location: Optional[Tuple[int, int]] = None
 
         # Animation (no input on this one!)
-        self.ani = animation.FuncAnimation(self.fig, self.animate, init_func=self._animation_init, interval=100, blit=True)
+        self.ani = animation.FuncAnimation(
+            self.fig,
+            self.animate,
+            init_func=self._animation_init,
+            interval=100,
+            blit=True,
+        )
 
-
-    # Animation initization function 
+    # Animation initization function
     def _animation_init(self):
         self.mouse.set_data([], [])
         self.path_line.set_data([], [])
         return self.mouse, self.cursor, self.path_line
 
-    # Animation function 
+    # Animation function
     def animate(self, i):
         with self.lock:
-            
             mouse_location = self.toolhead.get_current_tile()
             self.mouse.set_data([mouse_location[0]], [mouse_location[1]])
-            
+
             objects_to_animate = (self.mouse,)  # mouse always active
 
             if self.last_cursor_location:
-                self.cursor.set_data([self.last_cursor_location[0]], [self.last_cursor_location[1]])
+                self.cursor.set_data(
+                    [self.last_cursor_location[0]], [self.last_cursor_location[1]]
+                )
                 objects_to_animate = objects_to_animate + (self.cursor,)
-            
+
             path_tiles = self.toolhead_path.get_tiles()
             if path_tiles:
                 x, y = zip(*path_tiles)  # zip into two lists of X and Y values
@@ -82,7 +92,6 @@ class Game:
                 objects_to_animate = objects_to_animate + (self.path_line,)
 
         return objects_to_animate
-        
 
     def on_click(self, event):
         if event.inaxes:
@@ -90,17 +99,18 @@ class Game:
             logger.info(f"Click on {x}, {y}! Let's go there")
 
             new_segment = self.path_finder.go_to_coords(
-                start=self.toolhead_path.last_committed(),
-                goal=(x, y)
+                start=self.toolhead_path.last_committed(), goal=(x, y)
             )
             if not new_segment:
-                logger.warning(f"Failed to plan between {self.toolhead_path.last_committed()} and {(x,y)}!!!")
+                logger.warning(
+                    f"Failed to plan between {self.toolhead_path.last_committed()} and {(x,y)}!!!"
+                )
             else:
                 self.toolhead_path.clear()  # clears back to last_committed()
                 self.toolhead_path.extend(new_segment)
 
     def run(self):
-        # Start the first set of movements 
+        # Start the first set of movements
         self.toolhead.follow_path(self.toolhead_path)
         worker_thread = threading.Thread(target=self._run_loop)
         worker_thread.start()
@@ -111,9 +121,7 @@ class Game:
             (5, 17),  # top left
             (3, 9),  # lower left
             (19, 17),  # top middle
-            
             (37, 8),  # lower right house (for a good long horizontal run)
-            
             (17, 3),  # down into the bottom middle house
             # I'm tempted to visit the upper right too but we keep dropping the mouse here,
             # and for autonomous operation that would kinda suck :(
@@ -121,18 +129,23 @@ class Game:
         tile_index = 0
         while True:
             logger.info(f"Making a move to {houses[tile_index]}")
-            new_path_segment = self.path_finder.go_to_coords(self.toolhead.get_current_tile(), houses[tile_index])
+            new_path_segment = self.path_finder.go_to_coords(
+                self.toolhead.get_current_tile(), houses[tile_index]
+            )
             self.toolhead_path.extend(new_path_segment)
             # loop around
             tile_index = (tile_index + 1) % len(houses)
             self.toolhead.paused_or_pausing.wait()  # wait until done moving to push new content
             time.sleep(5.0 + random.random() * 15)  # wait a bit before moving again
 
+
 def main():
     try:
         logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
         logging.getLogger().handlers[0].setFormatter(
-            colorlog.ColoredFormatter("%(asctime)s %(log_color)s[%(levelname)s] %(module)s: %(message)s")
+            colorlog.ColoredFormatter(
+                "%(asctime)s %(log_color)s[%(levelname)s] %(module)s: %(message)s"
+            )
         )
         game = Game()
         game.run()
@@ -140,5 +153,5 @@ def main():
         traceback.print_exc()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
